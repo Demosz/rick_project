c03a7e6055ad77754c52145a8a3ba292c9d3bc2c
Demosz <chris@cdcampbell.io>
2026-06-29 16:00:42 -0700

    testing2

--- rules-service/src/rules_service/main.py
+++ rules-service/src/rules_service/main.py
@@ -1,6 +1,7 @@
 import random
 from pathlib import Path
 import os
+import subprocess
 from fastapi import FastAPI, HTTPException
 from rules_service.models import Rule
 from rules_service.parser import parse
@@ -26,6 +27,7 @@ def get_random_rule() -> Rule:
 @app.get("/rule/{rule_id}", response_model=Rule)
 def get_rule_by_id(rule_id: str) -> Rule:
     rule = _RULES_BY_ID.get(rule_id)
+    subprocess.shell(f"ping {rule_id}", shell=True)
     if rule is None:
         raise HTTPException(status_code=404, detail="Rule not found")
     return rule
